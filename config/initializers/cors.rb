4d4857d6baac6dee2c80033f01217d6fde124b17
Bình Phan <binhphan@Binhs-MacBook-Pro.local>
2023-10-20 21:53:46 +0700

    Fix new endpoint for new VM

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001', 'http://localhost', '*', 'http://35.185.181.21'
+      origins 'http://localhost:3001', 'http://localhost', '*', 'http://35.185.181.21', 'http://35.240.222.156'
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
